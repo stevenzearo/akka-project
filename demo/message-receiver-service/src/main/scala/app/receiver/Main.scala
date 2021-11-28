8312a766f42b0a28c9d326ad19fb07f63c44cb54
steve <2226069315@qq.com>
2021-11-28 16:44:21 +0800

    RouteStrategy Demo For Elastic

--- demo/message-receiver-service/src/main/scala/app/receiver/Main.scala
+++ demo/message-receiver-service/src/main/scala/app/receiver/Main.scala
@@ -13,8 +13,19 @@ object Main extends App {
     private val system: ActorSystem = ActorSystem("message-receiver", value)
     val connectionRef: ActorRef = system.actorOf(ConnectionContext.connectionContextProps, "connection-context")
     private val receiverProps = Props(classOf[MessageReceiver], connectionRef)
+    // config file def of route strategy, pool strategy will create a parent actor as supervisor
     private val routerProps: Props = FromConfig.props(receiverProps)
+
     private val receiverRef: ActorRef = system.actorOf(routerProps, "receiver")
+    // code def of route strategy
+//    private val receiverRef: ActorRef = system.actorOf(RoundRobinPool(5).props(receiverProps), "receiver")
+    /*val paths = List(
+        "receiver1",
+        "receiver2",
+        "receiver3")
+    private val actorPaths: List[String] = paths.map(p => system.actorOf(receiverProps, p).path.toString) // todo
+    private val receiverRef: ActorRef = system.actorOf(RoundRobinGroup(actorPaths).props(), "receiver") // todo
+*/
     private val log: LoggingAdapter = system.log
     log.info(receiverRef.path.toSerializationFormat)
 }
