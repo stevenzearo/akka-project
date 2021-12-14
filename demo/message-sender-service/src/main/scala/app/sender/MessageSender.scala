e3ffbd92af82ac54248333228d86d593917002ca
steve <2226069315@qq.com>
2021-12-15 00:06:43 +0800

    Persistent actor to mongo impl

--- demo/message-sender-service/src/main/scala/app/sender/MessageSender.scala
+++ demo/message-sender-service/src/main/scala/app/sender/MessageSender.scala
@@ -1,15 +1,22 @@
 package app.sender
 
-import akka.actor.{Actor, ActorLogging, ActorRef}
+import akka.actor.{ActorLogging, ActorRef}
+import akka.persistence.{AtLeastOnceDelivery, PersistentActor, RecoveryCompleted}
 import akka.serialization.Serialization
 import app.receiver.api.{CloseMessage, ConnectionMessage}
 
 import scala.sys.exit
 
-class MessageSender(server: ActorRef) extends Actor with ActorLogging {
+class MessageSender(server: ActorRef) extends PersistentActor with AtLeastOnceDelivery with ActorLogging {
+  var state: ConnectionMessage = _
+
   override def receive: Receive = {
     case msg: ConnectionMessage =>
       log.info(s"get message from ${msg.fromPath}: ${msg.content}")
+      // todo need to check logic
+      persist[ConnectionMessage](msg)(msg => {
+        state = msg
+      })
       log.info(s"Reply to ${msg.fromPath}:")
       val content = Console.in.readLine()
       if ("exit".equalsIgnoreCase(content)) {
@@ -20,4 +27,13 @@ class MessageSender(server: ActorRef) extends Actor with ActorLogging {
       }
     case _ => log.error("unknown message!")
   }
+
+  override def receiveRecover: Receive = {
+    case msg: ConnectionMessage => state = msg
+    case RecoveryCompleted => log.info(s"actor-$persistenceId recovery complete")
+  }
+
+  override def receiveCommand: Receive = receive
+
+  override def persistenceId: String = "sender-0001"
 }
