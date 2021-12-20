d82fab1d511b0e96702b47371094fc296b84e977
steve <2226069315@qq.com>
2021-12-20 15:14:00 +0800

    remove logic of loading property files

--- demo/message-receiver-service/src/main/scala/app/receiver/Main.scala
+++ demo/message-receiver-service/src/main/scala/app/receiver/Main.scala
@@ -3,14 +3,11 @@ package app.receiver
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.event.LoggingAdapter
 import akka.routing.FromConfig
-import com.typesafe.config.{Config, ConfigFactory}
-
-import java.io.File
 
 object Main extends App {
-  private val file: File = new File("./demo/message-receiver-service/src/main/resources/application.conf")
-  private val value: Config = ConfigFactory.parseFile(file)
-  private val system: ActorSystem = ActorSystem("message-receiver", value)
+  //  private val file: File = new File("./demo/message-receiver-service/src/main/resources/application.conf")
+  //  private val value: Config = ConfigFactory.parseFile(file)
+  private val system: ActorSystem = ActorSystem("message-receiver") // will automatically load application.conf
   val connectionRef: ActorRef = system.actorOf(ConnectionContext.connectionContextProps, "connection-context")
   private val receiverProps = Props(classOf[MessageReceiver], connectionRef)
   // config file def of route strategy, pool strategy will create a parent actor as supervisor
